59ca9583bea936e2e26f0f4f48ee59a16ec43432
satis.vishnu <satis.vishnu@gmail.com>
2016-09-28 01:23:13 +0530

    fixing small issue

--- api/application.py
+++ api/application.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, Flask
-from api.auth.basic_auth import basic_auth
 from api.handlers.v1_0.lin_reg_handler import lr_handler
 
 bp = Blueprint(__name__, __name__)
@@ -7,7 +6,6 @@ app = Flask(__name__)
 
 
 @bp.route('/')
-@basic_auth
 def index():
     return "Hello, World!"
 
