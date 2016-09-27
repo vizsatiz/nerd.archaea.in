e26509ff718616fae78ba3e3cb771adabfe1523d
satis.vishnu <satis.vishnu@gmail.com>
2016-09-28 01:18:53 +0530

    Completed the folder structure of nerd

--- api/constants/http_exceptions.py
+++
@@ -1,7 +0,0 @@
-
-HTTP_EXCEPTIONS = {
-
-    'BASIC_AUTH_FAILURE': '[Auth Failure] basic auth failure',
-    'REQUEST_ID_NOT_FOUND': '[Invalid Param] request id not found',
-    'CONTENT_TYPE_INVALID': '[Invalid Param] content type not valid'
-}
