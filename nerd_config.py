c1e6378eccc098d97a724fa4597a02133557e827
satis.vishnu <satis.vishnu@gmail.com>
2016-12-10 00:14:06 +0530

    Making changes for creating a json config file

--- nerd_config.py
+++ nerd_config.py
@@ -1,7 +1,10 @@
 import os
+import json
 
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+with open(PROJECT_ROOT + '/nerd.config.json') as config_file:
+    config = json.load(config_file)
 
 # Database Setup
-MY_SQL_HOST_URL = 'mysql://root:Kony@123@localhost/nerd'
+MY_SQL_HOST_URL = config['DATABASE_URL']
